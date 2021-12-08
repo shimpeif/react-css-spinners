4adf0ad9fda1f088dbd2727663f6030215386150
ernest <cafelashowerezweb@gmail.com>
2021-12-08 11:51:19 +0900

    set up rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const dist = 'dist'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: `${dist}/bundle.cjs.js`,
+      format: 'cjs'
+    },
+    {
+      file: `${dist}/bundle.esm.js`,
+      format: 'esm'
+    },
+    {
+      file: `${dist}/bundle.umd.js`,
+      format: 'umd'}
+  ]
+}
