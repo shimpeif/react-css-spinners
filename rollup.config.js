25c0f342dbc02b18811fefd1510283a55bb90f71
ernest <cafelashowerezweb@gmail.com>
2021-12-09 17:03:33 +0900

    set up babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,39 @@
-const dist = 'dist'
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const dist = 'dist';
+const bundle = 'bundle';
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/index.js',
+  external: ['react'],
   output: [
     {
-      file: `${dist}/bundle.cjs.js`,
+      file: `${dist}/${bundle}.cjs.js`,
       format: 'cjs'
     },
     {
-      file: `${dist}/bundle.esm.js`,
+      file: `${dist}/${bundle}.esm.js`,
       format: 'esm'
     },
     {
-      file: `${dist}/bundle.umd.js`,
-      format: 'umd'}
+      name: 'ReactCssSpinners',
+      file: `${dist}/${bundle}.umd.js`,
+      format: 'umd',
+      globals: {
+        react: 'React'
+      }
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+      babelHelpers: 'bundled'
+    }),
+    production && terser()
   ]
-}
+};
