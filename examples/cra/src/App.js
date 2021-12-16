e5b882b326fe32e14099b3bee3c9ba4383e78db8
ernest <cafelashowerezweb@gmail.com>
2021-12-16 20:07:56 +0900

    add ssr, cra and cdn examples

---
+++ examples/cra/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Heart, Ripple } from 'react-css-spinners';
+
+function App() {
+  return (
+    <div className="App">
+      <Ripple />
+      <Heart />
+    </div>
+  );
+}
+
+export default App;
