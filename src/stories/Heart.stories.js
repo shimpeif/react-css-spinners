75f59368527ba6d057d81860cf9ae31a9fa55648
ernest <cafelashowerezweb@gmail.com>
2021-12-29 20:21:09 +0900

    add storybook features

---
+++ src/stories/Heart.stories.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Heart } from '../';
+
+export default {
+  title: 'Heart',
+  component: Heart
+};
+
+export const HeartSpinner = () => <Heart />;
---
+++ src/stories/Ripple.stories.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Ripple } from '../';
+
+export default {
+  title: 'Ripple',
+  component: Ripple
+};
+
+export const RippleSpinner = () => <Ripple />;
