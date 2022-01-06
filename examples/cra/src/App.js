6154f78195da37a905d5f40cbe6afa76aa0acf8a
ernest <cafelashowerezweb@gmail.com>
2022-01-06 21:15:17 +0900

    update cra example to add hourglass spinner

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Heart, Ripple } from 'react-css-spinners';
+import { Heart, Ripple, Hourglass } from 'react-pure-css-spinners';
 
 function App() {
   return (
     <div className="App">
       <Ripple />
       <Heart />
+      <Hourglass />
     </div>
   );
 }
